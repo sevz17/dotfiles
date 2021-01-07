62b095a35ee5fa36266209f21c885c0e8b7d269a
Sevz17 <leohdz172@outlook.com>
2021-01-07 16:16:48 -0600

    Agregado script para la instalacion de zsh

---
+++ Instalacion.sh
@@ -0,0 +1,4 @@
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions /home/leonardo/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/leonardo/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions /home/leonardo/.oh-my-zsh/custom/plugins/zsh-completions
