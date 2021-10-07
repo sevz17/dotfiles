eaf524471e35498f85309afcbd0f4b4fcd592115
Leonardo Hernandez Hernandez <leohdz172@outlook.com>
2021-10-07 18:17:47 -0500

    neovim: add lua lsp

--- .config/nvim/lua/lsp-servers.lua
+++ .config/nvim/lua/lsp-servers.lua
@@ -1,6 +1,21 @@
 local nvim_lsp = require('lspconfig')
+local configs = require'lspconfig/configs'
 
-local servers = { 'rls', 'clangd', 'bashls', 'csharp_ls' }
+-- Check if it's already defined for when reloading this file.
+if not nvim_lsp.lua_lsp then
+  configs.lua_lsp = {
+    default_config = {
+      cmd = {'/usr/share/lua/5.1/bin/lua-lsp'};
+      filetypes = {'lua'};
+      root_dir = function(fname)
+        return nvim_lsp.util.find_git_ancestor(fname) or vim.loop.os_homedir()
+      end;
+      settings = {};
+    };
+  }
+end
+
+local servers = { 'rls', 'clangd', 'bashls', 'csharp_ls', 'lua_lsp' }
 
 for _, lsp in ipairs(servers) do
 	nvim_lsp[lsp].setup {
