002e72dec7f5870e0ee42905a7ea7292ff98db39
Leonardo Hernandez Hernandez <leohdz172@protonmail.com>
2022-01-10 00:06:16 -0600

    nvim: delete lua lsp

--- .config/nvim/lua/lsp-servers.lua
+++ .config/nvim/lua/lsp-servers.lua
@@ -1,23 +1,8 @@
 -- vi: expandtab ts=2 sw=2
 
 local nvim_lsp = require('lspconfig')
-local configs = require'lspconfig/configs'
 
--- Check if it's already defined for when reloading this file.
-if not nvim_lsp.lua_lsp then
-  configs.lua_lsp = {
-    default_config = {
-      cmd = {'/usr/share/lua/5.1/bin/lua-lsp'};
-      filetypes = {'lua'};
-      root_dir = function(fname)
-        return nvim_lsp.util.find_git_ancestor(fname) or vim.loop.os_homedir()
-      end;
-      settings = {};
-    };
-  }
-end
-
-local servers = { 'rls', 'clangd', 'bashls', 'csharp_ls', 'lua_lsp', 'pylsp' }
+local servers = { 'rls', 'clangd', 'bashls', 'csharp_ls', 'pylsp' }
 
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
