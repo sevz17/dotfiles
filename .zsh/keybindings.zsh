4fd65a3d2c9cd9caf5ba63b197bbff59ec269e2c
Leonardo Hernández Hernández <leohdz172@protonmail.com>
2022-02-26 14:37:31 -0600

    zsh: refactor: move keybindings to another file
    Signed-off-by: Leonardo Hernández Hernández <leohdz172@protonmail.com>

---
+++ .zsh/keybindings.zsh
@@ -0,0 +1,15 @@
+typeset -g -A key
+
+key[Shift-Tab]="${terminfo[kcbt]}"
+
+[[ -n "${key[Shift-Tab]}" ]] && \
+	bindkey -- "${key[Shift-Tab]}" reverse-menu-complete
+
+function exit_zsh() { exit }
+zle -N exit_zsh
+bindkey '^D' exit_zsh
+
+bindkey 'TAB' expand-or-complete-prefix
+
+bindkey '^P' up-line-or-search
+bindkey '^N' down-line-or-search
--- .zshrc
+++ .zshrc
@@ -49,10 +49,6 @@ function ch() {
   sudo chroot "${newroot:=/mnt/arch}" "${cmd:=/bin/zsh}"
 }
 
-exit_zsh() { exit }
-zle -N exit_zsh
-bindkey '^D' exit_zsh
-
 unset SSH_AGENT_PID
 if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
   export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
