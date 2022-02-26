926823b083d674d1d386065ea5a39950b21f80e1
Leonardo Hernández Hernández <leohdz172@protonmail.com>
2022-02-26 14:30:02 -0600

    zsh: refactor: move completions settings to another file
    Signed-off-by: Leonardo Hernández Hernández <leohdz172@protonmail.com>

---
+++ .zsh/completions.zsh
@@ -0,0 +1,41 @@
+zstyle ':completion:*' rehash true
+
+zstyle ':completion::complete:*' use-cache true
+zstyle ':completion:*' cache-path ${ZSH_CACHE_DIR}
+
+zstyle -e ':completion:*:hosts' hosts 'reply=(
+  ${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) 2>/dev/null)"}%%[#| ]*}//,/ }
+  ${=${${${${(@M)${(f)"$(cat ~/.ssh/config 2>/dev/null)"}:#Host *}#Host }:#*\**}:#*\?*}}
+)'
+
+# Highlight matching part of available completions
+zstyle ':completion:*:default' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*' list-colors  'reply=( "=(#b)(*$PREFIX)(?)*=00=$color[green]=$color[bg-green]" )'
+zstyle ':completion:*' select-prompt %SScrolling active: current selection at %P Lines: %m
+zstyle ':completion:*:corrections' format $'%{\e[0;31m%}%d (errors: %e)%}'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+
+# Autocompletion with an arrow-key driven interface
+zstyle ':completion:*' menu select
+
+# complete . and .. special directories
+zstyle ':completion:*' special-dirs true
+
+# don't complete backup files as executables
+zstyle ':completion:*:complete:-command-::commands' ignored-patterns '*\~'
+
+# case and hyphen insensitive path-completion 
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|=*' 'l:|=* r:|=*'
+
+# partial completion suggestions
+zstyle ':completion:*' list-suffixes
+zstyle ':completion:*' expand prefix
+
+# Prevent CVS files/directories from being completed:
+zstyle ':completion:*:(all-|)files' ignored-patterns '(|*/)CVS'
+zstyle ':completion:*:cd:*' ignored-patterns '(*/)#CVS'
+
+# ... unless we really want to.
+zstyle '*' single-ignored show
