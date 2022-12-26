b8203252be99cf7b9db1521e48ef140624d7bddf
Leonardo Hernández Hernández <leohdz172@protonmail.com>
2022-12-26 11:45:25 -0600

    fish: add integration with foot
    References: https://codeberg.org/dnkl/foot/wiki#fish-1

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,4 +3,8 @@ if status is-interactive
     zoxide init fish | source
 
     set -xg GPG_TTY $(tty)
+
+    function mark_prompt_start --on-event fish_prompt
+        echo -en "\e]133;A\e\\"
+    end
 end
