56595685b5f30e8aaa8aede4eac34588804a46fa
Leonardo Hernández Hernández <leohdz172@proton.me>
2023-05-02 21:24:45 -0600

    zsh: temporarily disable bindings for ^N and ^P

--- .config/zsh/keybindings.zsh
+++ .config/zsh/keybindings.zsh
@@ -16,7 +16,7 @@ autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 
-bindkey '^P' up-line-or-beginning-search
-bindkey '^N' down-line-or-beginning-search
+#bindkey '^P' up-line-or-beginning-search
+#bindkey '^N' down-line-or-beginning-search
 
 bindkey ' ' magic-space                               # [Space] - don't do history expansion
